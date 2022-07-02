f2f0de7d27fd2dd000d208e60b0c6a0fa4f86577
Bogahkiin <jonas.rct@gmail.com>
2022-07-02 00:22:50 -0300

    limpando e comentando código, atualizando README

--- 7daysofcode-2Dia.js
+++ 7daysofcode-2Dia.js
@@ -1,21 +1,27 @@
 //Segundo dia
+
 const botaoEnviar = document.getElementById("input-enviar");
 const nome = document.getElementById("nome");
 const idade = document.getElementById("idade");
 const linguagem = document.getElementById("linguagem");
 const mensagem = document.getElementById("resposta");
+
 //Formulário e mensagem de resposta
 botaoEnviar.onclick = function () {
+    
+    //Resposta com os dados preenchidos
     let dadosInseridos = [nome.value, idade.value, linguagem.value];
     const primeirasPerguntas = document.getElementById("primeiras-perguntas");
     mensagem.innerText = `Olá ${dadosInseridos[0]}, você tem ${dadosInseridos[1]} anos e já está aprendendo ${dadosInseridos[2]}!`;
     primeirasPerguntas.classList.add('sumir');
 
+    //Pergunta se está gostando
     const pergunta = document.getElementById("texto-pergunta");
     const fieldset = document.getElementById("perguntar-se-gosta");
     pergunta.innerText = `E está gostando de estudar ${dadosInseridos[2]}?`;
     fieldset.classList.remove('sumir');
 };
+
 //Desafio opcional
 const botaoSim = document.getElementById("botao-sim");
 const botaoNao = document.getElementById("botao-nao");
--- 7daysofcode-3Dia.js
+++ 7daysofcode-3Dia.js
@@ -1,15 +1,18 @@
 //Terceiro dia
+
+//Verificando qual botão foi apertado
 function opcaoEscolhida(algo) {
     let opcao;
     opcao = algo;
     for (i = 0; i < 8; i++) {
         if (opcao == i) {
             let idBotao = opcao;
-            console.log(idBotao);
             tirarSumirDaPergunta(idBotao);
         }
     }
 }
+
+//Fazendo a opção aparecer baseado na função anterior
 function tirarSumirDaPergunta(idPergunta) {
 
     const divFront = document.getElementById('perguntas-dia-3-front');
@@ -17,29 +20,27 @@ function tirarSumirDaPergunta(idPergunta) {
     const divFull = document.getElementById('perguntas-dia-3-full');
     const divFinal = document.getElementById('perguntas-dia-3-final');
 
-    console.log(`segunda vez ` + idPergunta);
+    let listaPerguntas = [divFront, divBack, divFull, divFinal]; //Tentando diminuir repetição no código
 
-    let listaPerguntas = [divFront, divBack, divFull, divFinal];
-
-    if (idPergunta == 0) {
-        console.log(`terceira vez ` + idPergunta);
+    if (idPergunta == 0) { //Talvez um for ()
         listaPerguntas[0].classList.remove('sumir');
-        listaPerguntas[1].classList.add('sumir');
+        listaPerguntas[1].classList.add('sumir'); //faz a mensagem do back-end desaperecer caso ela esteja aparecendo
     } else if (idPergunta == 1) {
         listaPerguntas[1].classList.remove('sumir');
-        listaPerguntas[0].classList.add('sumir');
+        listaPerguntas[0].classList.add('sumir'); //faz o inverso da de cima (front-end desaparece)
     } else if (idPergunta == 2 || idPergunta == 3) {
         listaPerguntas[2].classList.remove('sumir');
     } else if (idPergunta == 4) {
         listaPerguntas[3].classList.remove('sumir');
     }
 }
+
+//Última pergunta em que pode ser preenchida por palavras ao infinito
 function adicionarPalavraAoParagrafo() {
 
     const campoTexto = document.getElementById('ultima-pergunta-dia-3');
     const paragrafoLista = document.getElementById('lista-final');
     let palavrasInseridas = campoTexto.value;
 
-    console.log(palavrasInseridas);
     paragrafoLista.innerText += ` ${palavrasInseridas};`;
 }
