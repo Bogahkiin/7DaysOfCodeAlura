c343131a3964245e403b482678be03ebdb9e00cf
Bogahkiin <jonas.rct@gmail.com>
2022-07-01 23:38:51 -0300

    terceiro desafio concluido

--- 7daysofcode.js
+++ 7daysofcode.js
@@ -37,12 +37,12 @@ const mensagem = document.getElementById("resposta");
 botaoEnviar.onclick = function () {
     let dadosInseridos = [nome.value, idade.value, linguagem.value];
     const primeirasPerguntas = document.getElementById("primeiras-perguntas");
-    mensagem.innerHTML = `Olá ${dadosInseridos[0]}, você tem ${dadosInseridos[1]} anos e já está aprendendo ${dadosInseridos[2]}!`;
+    mensagem.innerText = `Olá ${dadosInseridos[0]}, você tem ${dadosInseridos[1]} anos e já está aprendendo ${dadosInseridos[2]}!`;
     primeirasPerguntas.classList.add('sumir');
 
     const pergunta = document.getElementById("texto-pergunta");
     const fieldset = document.getElementById("perguntar-se-gosta");
-    pergunta.innerHTML = `E está gostando de estudar ${dadosInseridos[2]}?`;
+    pergunta.innerText = `E está gostando de estudar ${dadosInseridos[2]}?`;
     fieldset.classList.remove('sumir');
 }
 
@@ -51,12 +51,58 @@ const botaoSim = document.getElementById("botao-sim");
 const botaoNao = document.getElementById("botao-nao");
 const segundaResposta = document.getElementById("segunda-resposta");
  botaoSim.onclick = function () {
-    segundaResposta.innerHTML = `Muito bom! Continue estudando e você terá muito sucesso.`
+    segundaResposta.innerText = `Muito bom! Continue estudando e você terá muito sucesso.`
  }
  botaoNao.onclick = function () {
-    segundaResposta.innerHTML = `Ahh que pena... Já tentou aprender outras linguagens?`
+    segundaResposta.innerText = `Ahh que pena... Já tentou aprender outras linguagens?`
  }
 
-//Segundo dia
+//Terceiro dia
 
+function opcaoEscolhida (algo) {
+    let opcao;
+    opcao = algo;
+    for (i = 0; i < 8; i++) {
+        if (opcao == i) {
+            let idBotao = opcao;
+            console.log(idBotao);
+            tirarSumirDaPergunta (idBotao);
+        }
+    }
+}
 
+function tirarSumirDaPergunta (idPergunta) {
+
+    const divFront = document.getElementById('perguntas-dia-3-front');
+    const divBack = document.getElementById('perguntas-dia-3-back');
+    const divFull = document.getElementById('perguntas-dia-3-full');
+    const divFinal = document.getElementById('perguntas-dia-3-final');
+
+    console.log(`segunda vez `+idPergunta);
+
+    let listaPerguntas = [divFront, divBack, divFull, divFinal];
+
+    if (idPergunta == 0) {
+        console.log(`terceira vez `+idPergunta);
+        listaPerguntas[0].classList.remove('sumir');
+        listaPerguntas[1].classList.add('sumir');
+    } else if (idPergunta == 1) {
+        listaPerguntas[1].classList.remove('sumir');
+        listaPerguntas[0].classList.add('sumir');
+    } else if (idPergunta == 2 || idPergunta == 3) {
+        listaPerguntas[2].classList.remove('sumir');
+    } else if (idPergunta == 4) {
+        listaPerguntas[3].classList.remove('sumir');
+    }
+}
+
+function adicionarPalavraAoParagrafo() {
+
+    const campoTexto = document.getElementById('ultima-pergunta-dia-3');
+    const botaoMais = document.getElementById('botao+');
+    const paragrafoLista = document.getElementById('lista-final');
+    let palavrasInseridas = campoTexto.value;
+
+    console.log(palavrasInseridas);
+    paragrafoLista.innerText += ` ${palavrasInseridas};`;
+}
